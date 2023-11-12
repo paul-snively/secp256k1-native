b25f1d183b5deca013ac1ba99979233d434863ea
Paul Snively <psnively@mac.com>
2023-11-12 15:04:47 -0500

    Pretty decent cut at cross-compiling for x86_64 macOS. Fails to find jni_md.h in 'darwin' subdirectory, so I need to fiddle with it a bit more.

--- compile.sh
+++ compile.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+dl_macos_jdks () {
+  MACOS_JDKS=`mktemp -d`
+  arches=('x64' 'aarch64')
+  for arch in ${arches[@]}; do
+    mkdir -p $MACOS_JDKS/$arch
+  done
+  printf "%s\n" "${arches[@]}" | xargs -I{} -P2 -- curl -LOs --output-dir $MACOS_JDKS/{} 'https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.20.1+1/OpenJDK11U-jdk_{}_mac_hotspot_11.0.20.1_1.tar.gz'
+  printf "%s\n" "${arches[@]}" | xargs -I{} -P2 -- env MACOS_JDKS=$MACOS_JDKS bash -c 'WORK_DIR=`echo $MACOS_JDKS/{}` && pushd $WORK_DIR >/dev/null && tar -xzf OpenJDK11U-jdk_{}_mac_hotspot_11.0.20.1_1.tar.gz && rm OpenJDK11U-jdk_{}_mac_hotspot_11.0.20.1_1.tar.gz && popd >/dev/null'
+}
+
+cleanup () {
+  echo "Deleting $MACOS_JDKS..."
+  rm -fr "$MACOS_JDKS"
+}
+
+trap cleanup EXIT
+
+# Make this smarter (i.e. don't re-download if already done)
+dl_macos_jdks
+
 cp -r secp256k1/ secp256k1-tmp/
 
 pushd secp256k1-tmp/
@@ -14,10 +34,17 @@ cp -r ../jni/java/ src/java/
 cp ../jni/Makefile.am Makefile.am
 cp ../jni/configure.ac configure.ac
 
+# Assumption: <https://github.com/tpoechtrager/osxcross> has been installed and is on the $PATH
+JNF="$(xcrun --show-sdk-path)/System/Library/Frameworks/JavaNativeFoundation.framework"
+unlink $JNF/Headers || true
+ln -s "$MACOS_JDKS/x64/jdk-11.0.20.1+1/Contents/Home/include" "$JNF/Headers"
+
 # Compile secp256k1 native code
 ./autogen.sh
-./configure --enable-jni --enable-module-ecdh --enable-experimental --enable-module-schnorrsig --enable-module-ecdsa-adaptor
+CC=o64-clang ./configure --host='x86_64-apple-darwin23' --enable-jni --enable-module-ecdh --enable-experimental --enable-module-schnorrsig --enable-module-ecdsa-adaptor
 # ./configure --enable-jni --enable-module-ecdh
 make CFLAGS="-std=c99"
 make
-make check
+
+# Actually checking would involve emulation of the cross-compiled host, e.g. with qemu. Out of scope.
+# make check
